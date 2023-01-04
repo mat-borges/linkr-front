75e886a81238eaec5ac75e4d596ee04f79c987bb
Mateus Borges <borgesm.work@gmail.com>
2023-01-03 22:19:51 -0300

    style: publish post container colors

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -5,3 +5,8 @@ export const accentColor = '#171717';
 export const textAccentColor = '#B7B7B7';
 
 export const detailColor = '#151515';
+
+export const publishColor = '#FFFFFF';
+export const textPublishColor = '#707070';
+export const inputPublishColor = '#EFEFEF';
+export const textInputPublishColor = '#949494';
