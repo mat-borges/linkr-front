9696b2f5fe5201e1316c0e37fc3daadca7684efd
Oscar Maia <oscarfgmaia@gmail.com>
2023-01-09 03:44:42 -0300

    feat: added userId and userImage to context

--- src/components/context/customer.js
+++ src/components/context/customer.js
@@ -8,10 +8,12 @@ export const CustomerContext = createContext()
 export const CustomerProvider = ({ children }) => {
 
     const [token, setToken] = useState("")
+    const [userId, setUserId] = useState("")
+    const [userImage, setUserImage] = useState("")
 
     return(
         <CustomerContext.Provider 
-            value={ {token, setToken} }>
+            value={ {token, setToken,userId, setUserId,userImage, setUserImage} }>
             {children}
         </CustomerContext.Provider>
     )
