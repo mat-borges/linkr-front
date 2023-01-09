772ee7a8ba2570f1e8ef44d6d0f9dc7a61e3cd1a
Mateus Borges <borgesm.work@gmail.com>
2023-01-08 21:05:27 -0300

    feat: search bar distinguishes search type (hashtag or user)

--- src/components/Header/SearchBar.js
+++ src/components/Header/SearchBar.js
@@ -1,19 +1,17 @@
-import { detailColor, publishColor, textPublishColor } from '../../constants/colors';
 import { useEffect, useState } from 'react';
 
 import { DebounceInput } from 'react-debounce-input';
-import { MagnifyingGlass } from 'react-loader-spinner';
-import TrendingBox from '../../components/TrendingBox.js';
+import Preview from './Preview';
 import axios from 'axios';
+import { detailColor } from '../../constants/colors';
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
 
 export default function SearchBar() {
   const [searchInput, setSearchInput] = useState('');
   const [searchFocus, setSearchFocus] = useState(false);
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
+  const [isVisible, setIsVisible] = useState(false);
   const minLength = 3;
 
   useEffect(() => {
@@ -24,8 +22,14 @@ export default function SearchBar() {
     };
     if (searchInput.length >= minLength) {
       setLoading(true);
+      const type = searchInput.includes('#') ? 'hashtag' : 'user';
+      let input = searchInput;
+      if (type === 'hashtag') {
+        input = input.replace('#', '');
+      }
+
       axios
-        .get(`${process.env.REACT_APP_API_BASE_URL}/search/${searchInput}`, config)
+        .get(`${process.env.REACT_APP_API_BASE_URL}/search/${type}/${input}`, config)
         .then((res) => {
           setLoading(false);
           setResults(res.data);
@@ -39,18 +43,9 @@ export default function SearchBar() {
     }
   }, [searchInput]);
 
-  function searchGo(e) {
-    e.preventDefault();
-    setSearchInput('');
-    navigate('/search');
-  }
-
-  function handleSearch(e) {
-    setSearchInput(e.target.value);
-  }
   return (
     <Search>
-      <form onSubmit={searchGo}>
+      <form onSubmit={(e) => e.preventDefault()}>
         <DebounceInput
           element={SearchBarInput}
           id='search-bar'
@@ -58,32 +53,23 @@ export default function SearchBar() {
           placeholder='Procurar'
           autoComplete='off'
           value={searchInput}
-          onChange={handleSearch}
-          onFocus={() => setSearchFocus(true)}
+          onChange={(e) => setSearchInput(e.target.value)}
+          onFocus={() => {
+            setSearchFocus(true);
+            setIsVisible(true);
+          }}
           onBlur={() => setSearchFocus(false)}
-          minLength={3}
+          minLength={minLength}
           debounceTimeout={300}
         />
-        <Preview display={searchFocus ? 'flex' : 'none'} show={results.length === 0 ? 'flex' : 'none'}>
-          <h2>{loading ? <MagnifyingGlass color={detailColor} /> : 'Nenhum resultado encontrado'}</h2>
-          <li>
-            <TrendingBox
-              background={publishColor}
-              color={textPublishColor}
-              hide={searchInput.length < minLength ? 'false' : 'true'}
-              width='fit-content'
-              placedAt={searchFocus === true && searchInput.length < minLength ? 'SearchBar' : 'none'}
-            />
-          </li>
-          {results?.map((result) => {
-            return (
-              <li>
-                {result.image ? <img src={result.image} alt='mock' /> : <></>}
-                <p>{result.name}</p>
-              </li>
-            );
-          })}
-        </Preview>
+        <Preview
+          results={results}
+          searchFocus={searchFocus}
+          isVisible={isVisible}
+          setIsVisible={setIsVisible}
+          loading={loading}
+          searchInput={searchInput}
+        />
       </form>
     </Search>
   );
@@ -117,46 +103,5 @@ const SearchBarInput = styled.input`
   }
   ::placeholder {
     color: inherit;
-  }
-`;
-
-const Preview = styled.ul`
-  z-index: 1;
-  position: absolute;
-  top: 0;
-  width: 90%;
-  height: fit-content;
-  min-height: 10rem;
-  padding: 3.5rem 1rem 1rem;
-  border-radius: 0.7rem;
-  background-color: white;
-  display: ${(props) => props.display};
-  flex-direction: column;
-  align-items: flex-start;
-  li {
-    display: flex;
-    align-items: center;
-    color: ${textPublishColor};
-    font-size: 1.1rem;
-    h1 {
-      font-size: 1.4rem;
-    }
-    img {
-      height: 2rem;
-      margin-right: 0.8rem;
-      border-radius: 50%;
-      object-fit: contain;
-    }
-  }
-  h2 {
-    width: 100%;
-    height: 100%;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-    display: ${(props) => props.show};
-    font-size: 1rem;
-    color: ${textPublishColor};
-    font-weight: 700;
   }
 `;
