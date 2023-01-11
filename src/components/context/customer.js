e0ba4ca59d6a8d8cbc100bdcce288a63e146acd7
Tadeuvelloso <tadeuvellososilva@gmail.com>
2023-01-11 15:58:30 -0300

    feat: following state in context and localstorage

--- src/components/context/customer.js
+++ src/components/context/customer.js
@@ -7,9 +7,10 @@ export const CustomerProvider = ({ children }) => {
   const [token, setToken] = useState('');
   const [userId, setUserId] = useState('');
   const [userImage, setUserImage] = useState('');
+  const [following, setFollowing] = useState([]);
 
   return (
-    <CustomerContext.Provider value={{ token, setToken, userId, setUserId, userImage, setUserImage }}>
+    <CustomerContext.Provider value={{ token, setToken, userId, setUserId, userImage, setUserImage, setFollowing, following }}>
       {children}
     </CustomerContext.Provider>
   );
