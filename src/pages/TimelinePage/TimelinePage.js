ef4384a8c7676e7e0ca511b06a558249d2d473af
Oscar Maia <oscarfgmaia@gmail.com>
2023-01-13 16:40:11 -0300

    feat: refresh button in timeline

--- src/pages/TimelinePage/TimelinePage.js
+++ src/pages/TimelinePage/TimelinePage.js
@@ -1,32 +1,74 @@
-import { accentColor, textBaseColor } from '../../constants/colors.js';
-import { useContext, useEffect, useState } from 'react';
-
-import { CustomerContext } from '../../components/context/customer.js';
-import PublishPost from '../../components/PublishPost.js';
-import SinglePost from '../../components/SinglePost.js';
-import { ThreeDots } from 'react-loader-spinner';
-import TrendingBox from '../../components/TrendingBox.js';
-import axios from 'axios';
-import styled from 'styled-components';
-import swal from 'sweetalert';
-import { useNavigate } from 'react-router';
+import { accentColor, textBaseColor } from "../../constants/colors.js";
+import { useContext, useEffect, useState } from "react";
+import useInterval from "use-interval";
+import { CustomerContext } from "../../components/context/customer.js";
+import PublishPost from "../../components/PublishPost.js";
+import { ThreeDots } from "react-loader-spinner";
+import TrendingBox from "../../components/TrendingBox.js";
+import axios from "axios";
+import styled from "styled-components";
+import swal from "sweetalert";
+import { useNavigate } from "react-router";
+import PostList from "../../components/PostList.js";
+import RefreshPostsButton from "../../components/RefreshPostsButton.js";
 
 export default function Timeline() {
   const [posts, setPosts] = useState();
   const [loadingPage, setLoadingPage] = useState(true);
   const [refreshPage, setRefreshPage] = useState(false);
   const [error, setError] = useState(false);
-const [follow, setFollow] = useState([])
+  const [follow, setFollow] = useState([]);
   const { setToken, setUserId, setUserImage } = useContext(CustomerContext);
   const navigate = useNavigate();
+  const [newPosts, setNewPosts] = useState([]);
+  const [delay, setDelay] = useState(null);
+
+  useInterval(() => {
+    console.log("use interval");
+    SearchForNewPosts(localStorage.getItem("token"));
+  }, delay);
+
+  async function SearchForNewPosts(token) {
+    const config = {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    };
+
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API_BASE_URL}/timeline`,
+        config
+      );
+      if (JSON.stringify(data) !== JSON.stringify(posts)) {
+        let diffArr = [];
+        for (let i = 0; i < data.length; i++) {
+          if (JSON.stringify(posts).includes(JSON.stringify(data[i]))) {
+            continue;
+          } else {
+            diffArr.push(data[i]);
+          }
+          setNewPosts(diffArr);
+        }
+      }
+    } catch (err) {
+      if (err.response.status === 404) {
+        swal("ERROR 404", "Não há posts nessa trend.", "info");
+      }
+    }
+  }
 
   useEffect(() => {
     setLoadingPage(true);
 
     if (!localStorage.token) {
-      swal('Usuário não logado!', 'Faça o login novamente para acessar suas informações.', 'error');
+      swal(
+        "Usuário não logado!",
+        "Faça o login novamente para acessar suas informações.",
+        "error"
+      );
       setLoadingPage(false);
-      navigate('/');
+      navigate("/");
     } else {
       setToken(localStorage.token);
       setUserId(localStorage.user_id);
@@ -37,12 +79,14 @@ const [follow, setFollow] = useState([])
         authorization: `Bearer ${localStorage.token}`,
       },
     };
-    setFollow(JSON.parse(localStorage.following))
+    setFollow(JSON.parse(localStorage.following));
 
     axios
       .get(`${process.env.REACT_APP_API_BASE_URL}/timeline`, config)
       .then((res) => {
         setPosts(res.data);
+        setNewPosts(res.data);
+        setDelay(15000);
         setLoadingPage(false);
       })
       .catch((err) => {
@@ -50,26 +94,36 @@ const [follow, setFollow] = useState([])
         setError(true);
       });
   }, [refreshPage, navigate, setToken, setUserId, setUserImage]);
+
+  function AddNewPostsToPostsList() {
+    const newPostsList = [...newPosts, ...posts];
+    setPosts(newPostsList);
+    setNewPosts(newPostsList);
+  }
+
   if (loadingPage === true) {
     return (
       <Main>
         <Title>timeline</Title>
         <Container>
           <LeftBox>
-            <PublishPost refreshPage={refreshPage} setRefreshPage={setRefreshPage} />
-            <div className='loading'>
+            <PublishPost
+              refreshPage={refreshPage}
+              setRefreshPage={setRefreshPage}
+            />
+            <div className="loading">
               <ThreeDots
-                height='80'
-                width='80'
-                radius='9'
+                height="80"
+                width="80"
+                radius="9"
                 color={textBaseColor}
-                ariaLabel='three-dots-loading'
+                ariaLabel="three-dots-loading"
                 wrapperStyle={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
                 }}
-                wrapperClassName='loading'
+                wrapperClassName="loading"
                 visible={true}
               />
             </div>
@@ -78,10 +132,10 @@ const [follow, setFollow] = useState([])
             <TrendingBox
               background={accentColor}
               color={textBaseColor}
-              hide='false'
-              width='20vw'
-              placedAt='TimelinePage'
-              setIsVisible={() => console.log('')}
+              hide="false"
+              width="20vw"
+              placedAt="TimelinePage"
+              setIsVisible={() => console.log("")}
             />
           </RightBox>
         </Container>
@@ -93,7 +147,10 @@ const [follow, setFollow] = useState([])
         <Title>timeline</Title>
         <Container>
           <LeftBox>
-            <PublishPost refreshPage={refreshPage} setRefreshPage={setRefreshPage} />
+            <PublishPost
+              refreshPage={refreshPage}
+              setRefreshPage={setRefreshPage}
+            />
             <ErrorMessage>
               Ocorreu um erro ao tentar buscar os posts, <br />
               atualize a página
@@ -103,10 +160,10 @@ const [follow, setFollow] = useState([])
             <TrendingBox
               background={accentColor}
               color={textBaseColor}
-              hide='false'
-              width='20vw'
-              placedAt='TimelinePage'
-              setIsVisible={() => console.log('')}
+              hide="false"
+              width="20vw"
+              placedAt="TimelinePage"
+              setIsVisible={() => console.log("")}
             />
           </RightBox>
         </Container>
@@ -118,39 +175,37 @@ const [follow, setFollow] = useState([])
         <Title>timeline</Title>
         <Container>
           <LeftBox>
-            <PublishPost refreshPage={refreshPage} setRefreshPage={setRefreshPage} />
+            <PublishPost
+              refreshPage={refreshPage}
+              setRefreshPage={setRefreshPage}
+            />
+            {JSON.stringify(newPosts) !== JSON.stringify(posts) ? (
+              <RefreshPostsButton
+                AddNewPostsToPostsList={AddNewPostsToPostsList}
+              >
+                {newPosts.length}, new posts, load more!
+              </RefreshPostsButton>
+            ) : (
+              false
+            )}
             {posts.length !== 0 ? (
-              posts.map((p) => (
-                <SinglePost
-                  key={p.posts_id}
-                  postOwner_id={p.user_id}
-                  link={p.link}
-                  description={p.description}
-                  md_description={p.md_description}
-                  md_title={p.md_title}
-                  md_image={p.md_image}
-                  image={p.image}
-                  name={p.name}
-                  posts_id={p.posts_id}
-                  refreshPage={refreshPage}
-                  setRefreshPage={setRefreshPage}
-                />
-              ))
-            ) : follow.length !== 0 ? 
+              <PostList posts={posts} />
+            ) : follow.length !== 0 ? (
               <ErrorMessage>No posts found from your friends</ErrorMessage>
-              :
-              <ErrorMessage>You don't follow anyone yet. Search for new friends!</ErrorMessage>
-            
-            }
+            ) : (
+              <ErrorMessage>
+                You don't follow anyone yet. Search for new friends!
+              </ErrorMessage>
+            )}
           </LeftBox>
           <RightBox>
             <TrendingBox
               background={accentColor}
               color={textBaseColor}
-              hide='false'
-              width='20vw'
-              placedAt='TimelinePage'
-              setIsVisible={() => console.log('')}
+              hide="false"
+              width="20vw"
+              placedAt="TimelinePage"
+              setIsVisible={() => console.log("")}
             />
           </RightBox>
         </Container>
@@ -193,7 +248,7 @@ const ErrorMessage = styled.p`
   font-style: italic;
   font-weight: 400;
   font-size: 1.15rem;
-  font-family: 'Oswald', sans-serif;
+  font-family: "Oswald", sans-serif;
   text-align: center;
 `;
 
@@ -203,7 +258,7 @@ const Title = styled.h1`
   color: ${textBaseColor};
   font-weight: 700;
   font-size: 1.95rem;
-  font-family: 'Oswald', sans-serif;
+  font-family: "Oswald", sans-serif;
   @media (min-width: 660px) {
     margin-top: 4.6rem;
     margin-left: 0;
